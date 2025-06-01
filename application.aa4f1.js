c721a5925db717dc7fc27b29eedbaefff47d0e32
sboy61017 <sboy61017@gamil.com>
2025-06-01 13:18:43 +0800

    打通免費遊戲，修改小豬圖片問題

--- application.aa4f1.js
+++ application.aa4f1.js
@@ -14,7 +14,7 @@ System.register([], function (_export, _context) {
       _export("Application", Application = /*#__PURE__*/function () {
         function Application() {
           _classCallCheck(this, Application);
-          this.settingsPath = 'src/settings.4dd26.json';
+          this.settingsPath = 'src/settings.d8070.json';
           this.showFPS = true;
         }
         _createClass(Application, [{
--- index.49898.js
+++ index.49898.js
@@ -1,4 +1,4 @@
-System.register(["./application.370db.js"], function (_export, _context) {
+System.register(["./application.aa4f1.js"], function (_export, _context) {
   "use strict";
 
   var Application, canvas, $p, bcr, application;
