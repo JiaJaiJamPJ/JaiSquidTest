5df0a3152d307333ef3a83c20b249387d040c1df
sboy61017 <sboy61017@gamil.com>
2025-06-04 21:41:13 +0800

    Bonus接G v2版本

--- index.a6371.js
+++ index.a6371.js
@@ -1,4 +1,4 @@
-System.register(["./application.0b629.js"], function (_export, _context) {
+System.register(["./application.7d534.js"], function (_export, _context) {
   "use strict";
 
   var Application, canvas, $p, bcr, application;
