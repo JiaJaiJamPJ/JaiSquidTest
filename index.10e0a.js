edc2bbe958fae9769781d54e4b84c7fd9057d057
sboy61017 <sboy61017@gamil.com>
2025-06-07 14:49:37 +0800

    return一些bug卡死的地方，新增同步server後才可開始、修正一些bug

--- index.10e0a.js
+++ index.10e0a.js
@@ -1,4 +1,4 @@
-System.register(["./application.b8f32.js"], function (_export, _context) {
+System.register(["./application.881b4.js"], function (_export, _context) {
   "use strict";
 
   var Application, canvas, $p, bcr, application;
