f29a632f043619560770387afdf71b51ec326896
sboy61017 <sboy61017@gamil.com>
2025-06-07 23:17:56 +0800

    QA棒棒側處機掰問題 還沒同步時送出兩次指令

--- application.ee8c3.js
+++ application.ee8c3.js
@@ -14,7 +14,7 @@ System.register([], function (_export, _context) {
       _export("Application", Application = /*#__PURE__*/function () {
         function Application() {
           _classCallCheck(this, Application);
-          this.settingsPath = 'src/settings.a5579.json';
+          this.settingsPath = 'src/settings.75c9c.json';
           this.showFPS = true;
         }
         _createClass(Application, [{
--- index.bec8f.js
+++ index.bec8f.js
@@ -1,4 +1,4 @@
-System.register(["./application.08bec.js"], function (_export, _context) {
+System.register(["./application.ee8c3.js"], function (_export, _context) {
   "use strict";
 
   var Application, canvas, $p, bcr, application;
