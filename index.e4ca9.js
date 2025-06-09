e89f75617982514bf0b2f3ac32c6cf9ff37d4bf1
sboy61017 <sboy61017@gamil.com>
2025-06-10 07:20:50 +0800

    測試BUG版本

--- index.e4ca9.js
+++ index.e4ca9.js
@@ -1,4 +1,4 @@
-System.register(["./application.ee8c3.js"], function (_export, _context) {
+System.register(["./application.7db88.js"], function (_export, _context) {
   "use strict";
 
   var Application, canvas, $p, bcr, application;
