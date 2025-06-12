86f8f5740aaae5d8cdb4c442936de96d661ddc96
sboy61017 <sboy61017@gamil.com>
2025-06-12 21:59:50 +0800

    分數計算測試版本

--- index.1d7ec.js
+++ index.1d7ec.js
@@ -1,4 +1,4 @@
-System.register(["./application.7db88.js"], function (_export, _context) {
+System.register(["./application.70933.js"], function (_export, _context) {
   "use strict";
 
   var Application, canvas, $p, bcr, application;
