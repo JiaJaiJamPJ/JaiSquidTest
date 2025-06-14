84bd332517295b60b06a1b22176ddb3501b04a94
sboy61017 <sboy61017@gamil.com>
2025-06-14 15:15:59 +0800

    更新

--- index.91595.js
+++ index.91595.js
@@ -1,4 +1,4 @@
-System.register(["./application.59e20.js"], function (_export, _context) {
+System.register(["./application.8ae5f.js"], function (_export, _context) {
   "use strict";
 
   var Application, canvas, $p, bcr, application;
