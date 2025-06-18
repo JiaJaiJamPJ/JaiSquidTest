f51cfc1ec029ba90bc7a09ec42a55603d3874182
sboy61017 <sboy61017@gamil.com>
2025-06-18 12:31:59 +0800

    更新一些小BUG

--- index.bdfab.js
+++ index.bdfab.js
@@ -1,4 +1,4 @@
-System.register(["./application.8f6c4.js"], function (_export, _context) {
+System.register(["./application.b36f3.js"], function (_export, _context) {
   "use strict";
 
   var Application, canvas, $p, bcr, application;
