c6d389ebed94fc3f7e9bbf0578fbb810ed9b2b11
sboy61017 <sboy61017@gamil.com>
2025-06-22 16:30:07 +0800

    修正巷木
    修改項目：
    美術圖資源替換（小遊戲１２３背景換宇宙、金珠的動畫替換）
    小遊戲功能重構
    小遊戲Auto完善

--- index.591c4.js
+++ index.591c4.js
@@ -1,4 +1,4 @@
-System.register(["./application.42f27.js"], function (_export, _context) {
+System.register(["./application.294fd.js"], function (_export, _context) {
   "use strict";
 
   var Application, canvas, $p, bcr, application;
